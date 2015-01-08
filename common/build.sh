00ec6af20b2553d88bccde0f4dcd808aee7e3cbb
ZhangYongqiang <efgod@126.com>
2015-01-08 15:25:38 +0800

    1 fix build error

--- common/build.sh
+++ common/build.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+cp -fr ../proto . 
+
+protoc  proto/pair.proto --cpp_out=proto -I=proto
+mv proto/pair.pb.cc src/pair.pb.cpp
+
+protoc  proto/message.proto --cpp_out=proto -I=proto
+mv proto/message.pb.cc src/message.pb.cpp
+
+protoc  proto/session.proto --cpp_out=proto -I=proto
+mv proto/session.pb.cc src/session.pb.cpp
+
+cp -f proto/*.pb.* include/
+
+rm -fr proto
+
 mkdir -p lib
 rm -fr CMakeCache.txt
 rm -fr CMakeFiles
