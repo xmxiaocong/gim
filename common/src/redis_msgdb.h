a937c7eba5b0fbafda93817e5d0ac1484a9708e9
qiuhui <qiuhui8507@163.com>
2015-01-09 11:10:37 +0800

    rename redis_mb to redis_msgdb

---
+++ common/src/redis_msgdb.h
@@ -0,0 +1,68 @@
+#ifndef __REDIS_ADAPTOR_H__
+#define __REDIS_ADAPTOR_H__
+
+#include "mb_adaptor.h"
+#include "redis_cg.h"
+
+namespace gim {
+
+using namespace std;
+using namespace ef;
+
+#define DEFAULT_MB_CAPACITY 100
+#define DEFAULT_MSG_EXPIRY_TIME  60 * 60 * 24 * 7 /* a week */
+
+class RedisMsgDB : public MbAdaptor {
+public:
+	RedisMsgDB(const Json::Value &config);
+
+	// Caution! You must bind a valid RedisCG handler before any other operation
+	int bindCG(RedisCG *cg);
+
+	int size(const string &mbName);
+	
+	int sizeFrom(const string &mbName, int64 bMsgId);
+
+	int addMsg(const string &mbName, Message &msg);
+
+	int getMsgs(const string &mbName, int64 bMsgId, int length,
+			vector<Message> &msg);
+
+	int getMsgsForward(const string &mbName, 
+			int64 bMsgId, int length, vector<Message> &msg);
+
+	int delMsg(const string &mbName, int64 msgId);
+
+	int delMsgsBackward(const string &mbName, int64 bMsgId, int length = -1);
+
+	int delMsgs(const string &mbName, int64 bMsgId, int length = -1);
+
+	int clear(const string &mbName);
+
+	int incrId(const string &mbName, int64 &newId);
+
+	int getMsgId(const string &mbName, int64 &recentReadId);
+
+	int setMsgId(const string &mbName, int64 recentReadId);
+private:
+	RedisCG *m_cg;
+
+	Json::Value m_cfg;
+
+	int m_expiry;
+
+	int m_capacity;
+};
+
+class RdsMbFactory : public MbAdptFactory {
+public:
+	MbAdaptor *createNewMbAdpt(const Json::Value &config)
+	{
+		return new RedisMsgDB(config);
+	}
+};
+
+}
+	
+
+#endif //__REDIS_ADAPTOR_H__
